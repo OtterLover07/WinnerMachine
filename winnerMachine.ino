a06f6d13c1ec5e046867f83a809f75d56a980484
OtterLover07 <melker@willforss.se>
2025-01-13 11:50:00 +0100

    Intergration Hell
    Försöker fixa så att jag kan displaya åäö utan seriöst modifierade strängar

--- winnerMachine.ino
+++ winnerMachine.ino
@@ -8,11 +8,20 @@
 // Include Libraries
 #include <RTClib.h>
 #include <Wire.h>
+#include <SPI.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+#include <Adafruit_HX8357.h>
+// #include <U8g2_for_Adafruit_GFX.h>
+// #include <u8g2_fonts.h>
 
 // Init constants
-const String people[13]{"Gargamel", "Bolibompadraken", "Dr Doofenschmirtz", "Musse Pigg", "Snurre Sprätt", "Bamse", "Hulken", "Batman", "Donald J. Trump", "Donkey Kong", "Waluigi", "Mr. Bean", "Perry the Platypus"};
-const String contests[10]{"Schack", "Monopol", "Kubb", "Ett Maraton", "Kurragömma", "Längdhopp", "Paintball", "Stirrtävling", "Korvätning", "Boxning"};
+// "<>=" = "åäö"
+const String winners[13]{"Gargamel", "Bolibompadraken", "Dr Doofenshmirtz", "Musse Pigg", "Snurre Sprätt", "Bamse", "Hulken", "Batman", "Donald J. Trump", "Donkey Kong", "Waluigi", "Mr. Bean", "Perry the Platypus"};
+const String contests[10]{"Schack", "Monopol", "Kubb", "Ett Maraton", "Kurrag<>=mma", "Längdhopp", "Paintball", "Stirrtävling", "Korvätning", "Boxning"};
 
 // Init global variables
 int charVotes[13]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -23,22 +32,31 @@ int contest[3]{0, 0, 0};
 RTC_DS3231 rtc;
 #define CLOCK_INTERRUPT_PIN 2
 
+#define TFT_CS 10
+#define TFT_DC 9
+#define TFT_RST 8
+
+Adafruit_HX8357 display = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
+// U8G2_FOR_ADAFRUIT_GFX u8g;
+
+
 void setup() {
   // init communication
   Serial.begin(9600);
 
   // Init Hardware
-  rtcSetup();
   randomSeed(analogRead(A5));
+  rtcSetup();
+  lcdSetup();
 }
 
 void loop() {
   checkAlarm();
-  delay(1000);
+  delay(10000);
 }
 
 void onAlarm() {
-  Serial.println(winners[random(0, 12)]);
+  display.println(winners[random(0, 12)]);
 }
 
 void setAlarm() {
@@ -63,6 +81,25 @@ void checkAlarm() {
       setAlarm();
     }
   }
+}
+
+void lcdSetup() {
+  display.begin();
+  display.setRotation(1);
+  display.fillScreen(HX8357_BLACK);
+  display.setCursor(0, 50);
+  display.setTextColor(HX8357_WHITE); display.setTextSize(3);
+  display.println(contests[4]);
+
+  // display.begin();
+  // display.fillScreen(0x0000);
+  // u8g.begin(display);
+  // u8g.setFontMode(1);
+  // u8g.setFontDirection(1);
+  // u8g.setFont(u8g2_font_helvR14_tf);
+  // u8g.setForegroundColor(0xFFFF);
+  // u8g.setCursor(0, 0);
+  // u8g.print(contests[4]);
 }
 
 void rtcSetup() {
