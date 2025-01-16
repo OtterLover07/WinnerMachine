7e9dc461270f20705e0168c7c71f67c31e58d365
OtterLover07 <melker@willforss.se>
2025-01-16 15:26:50 +0100

    ÅÄÖ FUNKAR
    HELVETET ÄR ÖVER

--- winnerMachine.ino
+++ winnerMachine.ino
@@ -8,20 +8,15 @@
 // Include Libraries
 #include <RTClib.h>
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_GrayOLED.h>
-#include <Adafruit_SPITFT.h>
-#include <Adafruit_SPITFT_Macros.h>
-#include <gfxfont.h>
 #include <Adafruit_HX8357.h>
-// #include <U8g2_for_Adafruit_GFX.h>
-// #include <u8g2_fonts.h>
+// #include <U8g2_for_Adafruit_GFX.h> //U8g2-addon
+// #include <u8g2_fonts.h> //U8g2-addon
 
 // Init constants
-// "<>=" = "åäö"
-const String winners[13]{"Gargamel", "Bolibompadraken", "Dr Doofenshmirtz", "Musse Pigg", "Snurre Sprätt", "Bamse", "Hulken", "Batman", "Donald J. Trump", "Donkey Kong", "Waluigi", "Mr. Bean", "Perry the Platypus"};
-const String contests[10]{"Schack", "Monopol", "Kubb", "Ett Maraton", "Kurrag<>=mma", "Längdhopp", "Paintball", "Stirrtävling", "Korvätning", "Boxning"};
+// "åäö" = "<=>"
+const String winners[] {"Gargamel", "Bolibompadraken", "Dr Doofenshmirtz", "Musse Pigg", "Snurre Spr=tt", "Bamse", "Hulken", "Batman", "Donald J. Trump", "Donkey Kong", "Waluigi", "Mr. Bean", "Perry the Platypus"};
+const String contests[] {"Schack", "Monopol", "Kubb", "Ett Maraton", "Kurrag>mma", "Längdhopp", "Paintball", "Stirrt=vling", "Korv=tning", "Boxning"};
 
 // Init global variables
 int charVotes[13]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -36,8 +31,8 @@ RTC_DS3231 rtc;
 #define TFT_DC 9
 #define TFT_RST 8
 
-Adafruit_HX8357 display = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
-// U8G2_FOR_ADAFRUIT_GFX u8g;
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
+// U8G2_FOR_ADAFRUIT_GFX u8g; //U8g2-addon
 
 
 void setup() {
@@ -48,18 +43,24 @@ void setup() {
   randomSeed(analogRead(A5));
   rtcSetup();
   lcdSetup();
+
+  Serial.println("Setup Complete!");
 }
 
 void loop() {
   checkAlarm();
-  delay(10000);
+}
+
+void newContest(){
+  blabla
 }
 
 void onAlarm() {
-  display.println(winners[random(0, 12)]);
+  newContest();
+  Serial.println("Alarm!");
 }
 
-void setAlarm() {
+void checkAlarm() {
   if (rtc.alarmFired(1)) {
     rtc.clearAlarm(1);
 
@@ -71,35 +72,10 @@ void setAlarm() {
   }
 }
 
-void checkAlarm() {
-  if (rtc.alarmFired(1)) {
-    rtc.clearAlarm(1);
-
-    if (!rtc.setAlarm1(rtc.now() + TimeSpan(2), DS3231_A1_Second)) {
-      Serial.println("Error, alarm wasn't set!");
-    } else {
-      setAlarm();
-    }
-  }
-}
-
 void lcdSetup() {
-  display.begin();
-  display.setRotation(1);
-  display.fillScreen(HX8357_BLACK);
-  display.setCursor(0, 50);
-  display.setTextColor(HX8357_WHITE); display.setTextSize(3);
-  display.println(contests[4]);
-
-  // display.begin();
-  // display.fillScreen(0x0000);
-  // u8g.begin(display);
-  // u8g.setFontMode(1);
-  // u8g.setFontDirection(1);
-  // u8g.setFont(u8g2_font_helvR14_tf);
-  // u8g.setForegroundColor(0xFFFF);
-  // u8g.setCursor(0, 0);
-  // u8g.print(contests[4]);
+  tft.begin();
+  tft.fillScreen(HX8357_BLACK);
+  tft.setRotation(1);
 }
 
 void rtcSetup() {
@@ -122,6 +98,5 @@ void rtcSetup() {
   rtc.writeSqwPinMode(DS3231_OFF);
   rtc.disableAlarm(2);
 
-  setAlarm();
-  Serial.println("Setup Complete!");
+  checkAlarm();
 }
