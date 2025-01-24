bc268768b606ccf379226eb2e2728f00fb3920ff
OtterLover07 <melker@willforss.se>
2025-01-24 11:14:19 +0100

    Lots of progress
    * Voting kinda works
    * Display works
    * Timers adjustable
    * Contest setup works

--- winnerMachine.ino
+++ winnerMachine.ino
@@ -2,24 +2,29 @@
 * Name: "Vem Vinner?"-Maskinen
 * Author: Melker Willforss
 * Date: 2024-12-19
-* Description: 
+* Description:
+* Randomly selects two contestants (from [winners]) and one contest (from [contests]) each day, and displays it on a screen.
+* Then collects votes from buttons during the day, and selects a winner based on the votes at the end of the day.
 */
 
 // Include Libraries
 #include <RTClib.h>
 #include <Wire.h>
+#include <Adafruit_BusIO_Register.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_HX8357.h>
 
 // Init constants
 // "åäö" = "<=>"
-const String winners[13] {"Gargamel", "Bolibompadraken", "Dr Doofenshmirtz", "Musse Pigg", "Snurre Spr=tt", "Bamse", "Hulken", "Batman", "Donald J. Trump", "Donkey Kong", "Waluigi", "Mr. Bean", "Perry the Platypus"};
-const String contests[10] {"Schack", "Monopol", "Kubb", "Ett Maraton", "Kurrag>mma", "Längdhopp", "Paintball", "Stirrt=vling", "Korv=tning", "Boxning"};
+const String winners[13]{ "Gargamel", "Bolibompadraken", "Dr Doofenshmirtz", "Musse Pigg", "Snurre Spr=tt", "Bamse", "Hulken", "Batman", "Donald J. Trump", "Donkey Kong", "Waluigi", "Mr. Bean", "Perry the Platypus" };
+const String contests[10]{ "Schack", "Monopol", "Kubb", "Ett Maraton", "Kurrag>mma", "L=ngdhopp", "Paintball", "Stirrt=vling", "Korv=tning", "Boxning" };
+const int vote1 = 6, vote2 = 7;
 
 // Init global variables
-int charVotes[13]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int tempVotes[2]{0, 0};
-int contest[3]{0, 0, 0};
+int charVotes[13]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int tempVotes[2]{ 0, 0 };
+int current[3]{ 0, 0, 0 };
+bool contestActive = false;
 
 // construct objects
 RTC_DS3231 rtc;
@@ -36,57 +41,141 @@ void setup() {
   Serial.begin(9600);
 
   // Init Hardware
+  pinMode(vote1, INPUT_PULLUP);
+  pinMode(vote2, INPUT_PULLUP);
   randomSeed(analogRead(A5));
+  tft.begin();
+  tft.fillScreen(HX8357_BLACK);
+  tft.setRotation(3);
   rtcSetup();
-  lcdSetup();
 
-  Serial.println("Setup Complete!");
-  newContest();
+  Serial.println(F("Setup Complete!"));
+  tft.setCursor(0, 0);
+  tft.print(F("Setup Complete!"));
 }
 
 void loop() {
-  
+  while (!contestActive) {
+    checkAlarm();
+  }  //Wait until a contest begins
+
+  while (contestActive) {
+    if (digitalRead(vote1) == LOW) {
+      tempVotes[0] += 1;
+      while (digitalRead(vote1) == LOW) {
+        delay(0);
+      }
+      Serial.println(F("Vote registered!"));
+    } else if (digitalRead(vote2) == LOW) {
+      tempVotes[1] += 1;
+      while (digitalRead(vote2) == LOW) {
+        delay(0);
+      }
+      Serial.println(F("Vote registered!"));
+    }
+    checkAlarm();
+  }
 }
 
-void newContest(){
-  String temp = "";
+void finishContest() {
   tft.fillScreen(HX8357_BLACK);
-  tft.setCursor(50, 30);  tft.setTextSize(6);  tft.setTextColor(HX8357_MAGENTA);
-  tft.print(F("Vem Vinner?"));
-  temp = winners[random(0, 12)];
-  tft.setCursor(centerText(temp), 110);  tft.setTextSize(3);  tft.setTextColor(HX8357_WHITE);
-  tft.print(temp);
+  Serial.print("Final Votes: "); Serial.print(tempVotes[0]); Serial.print(", "); Serial.println(tempVotes[1]);
+  int winner;
+  if (tempVotes[0] > tempVotes[1]) {
+    winner = 0;
+  } else if (tempVotes[0] < tempVotes[1]) {
+    winner = 1;
+  } else {
+    winner = 1;
+  }
+
+  int y = 15;
+
+  tft.fillScreen(HX8357_BLACK);
+  tft.setTextSize(5); tft.setTextColor(HX8357_MAGENTA);
+  tft.setCursor(centerText(winners[current[winner]] + F(" Vinner!")), y); y += 85;
+  tft.println(winners[current[winner]] + F(" Vinner!"));
+  // tft.setTextSize(5); tft.setTextColor(HX8357_YELLOW);
+  // tft.setCursor(centerText(winners[temp1]), y);  y += 45;
+  // tft.print(winners[temp1]);
+  // tft.setTextColor(HX8357_WHITE);
+  // tft.setCursor(centerText(F("VS")), y);  y += 40;
+  // tft.print(F("VS"));
+  // tft.setTextColor(HX8357_GREEN);
+  // tft.setCursor(centerText(winners[temp2]), y);  y += 75;
+  // tft.print(winners[temp2]);
+  // tft.setTextSize(6); tft.setTextColor(HX8357_WHITE);
+  // tft.setCursor(centerText(contests[temp3]), y);
+  // tft.print(contests[temp3]);
+
+  for (int i = 0; i < 2; i++) { // Transfer votes from finished contest to total vote tally
+    charVotes[current[i]] += tempVotes[i];
+    tempVotes[i] = 0;
+  }
+  contestActive = false;
+  Serial.println(F("Contest Finished"));
+}
+
+void newContest() {
+  // Randomize contestants and contest
+  int temp1 = random(0, 12);
+  int temp2 = random(0, 12);
+  int temp3 = random(0, 9);
+  while (temp2 == temp1) {  // Make sure a contestant is not fighting themself
+    temp1 = random(0, 12);
+  }
+  current[0] = temp1;
+  current[1] = temp2;
+  current[2] = temp3;
+
+  int y = 15;
+
+  tft.fillScreen(HX8357_BLACK);
+  tft.setTextSize(7); tft.setTextColor(HX8357_MAGENTA);
+  tft.setCursor(centerText(F("Vem Vinner?")), y); y += 85;
+  tft.println(F("Vem Vinner?"));
+  tft.setTextSize(5); tft.setTextColor(HX8357_YELLOW);
+  tft.setCursor(centerText(winners[temp1]), y);  y += 45;
+  tft.print(winners[temp1]);
+  tft.setTextColor(HX8357_WHITE);
+  tft.setCursor(centerText(F("VS")), y);  y += 40;
+  tft.print(F("VS"));
+  tft.setTextColor(HX8357_GREEN);
+  tft.setCursor(centerText(winners[temp2]), y);  y += 75;
+  tft.print(winners[temp2]);
+  tft.setTextSize(6); tft.setTextColor(HX8357_WHITE);
+  tft.setCursor(centerText(contests[temp3]), y);
+  tft.print(contests[temp3]);
+
+  contestActive = true;
 }
 
 void onAlarm() {
   Serial.println("Alarm!");
+  if (!contestActive) {
+    newContest();
+  } else {
+    finishContest();
+  }
 }
 
-int centerText(String text){
-  int x;
-  int y;
-  unsigned int w;
-  unsigned int h;
-  tft.getTextBounds(text, 0, 0, &x, &y, &w, &h);
-  return((480-w)/2);
+int centerText(const String text) {
+  int16_t x1, y1;
+  uint16_t w, h;
+  tft.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);  //calc width of new string
+  Serial.println((480 - w) / 2);
+  return ((480 - w) / 2);
 }
 
 void checkAlarm() {
   if (rtc.alarmFired(1)) {
     rtc.clearAlarm(1);
 
-    if (!rtc.setAlarm1(rtc.now() + TimeSpan(10), DS3231_A1_Second)) {
+    if (!rtc.setAlarm1(rtc.now() + TimeSpan(60), DS3231_A1_Second)) {
       Serial.println("Error, alarm wasn't set!");
     } else {
-      Serial.println("");
     }
   }
-}
-
-void lcdSetup() {
-  tft.begin();
-  tft.fillScreen(HX8357_BLACK);
-  tft.setRotation(1);
 }
 
 void rtcSetup() {
